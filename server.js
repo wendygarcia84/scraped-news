025bfbcb5caabdf17806b548ad22a35933c84262
Wendy Garcia <wendy.fern84@gmail.com>
2019-10-22 14:36:59 -0700

    done

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var logger = require("morgan");
 var mongoose = require("mongoose");
 
 var db = require("./models");
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 // Initialize Express
 var app = express();
 // Configure middleware
@@ -18,7 +18,8 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/scrapednews", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapednews";
+mongoose.connect(MONGODB_URI);
 
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
